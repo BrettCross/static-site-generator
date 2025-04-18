eb16b63fba38f6eaf8e725061c0bac3516db10ca
Brett Cross <9117646+BrettCross@users.noreply.github.com>
2025-04-17 22:57:16 -0400

    add image and link raw markdown extraction

--- src/markdown_inline.py
+++ src/markdown_inline.py
@@ -1,3 +1,5 @@
+import re
+
 from textnode import TextNode, TextType
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -35,3 +37,20 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 new_nodes.append(TextNode(text, text_type))
 
     return new_nodes
+
+
+def extract_markdown_images(text):
+    """takes raw markdown text as input and returns a list of tuples,
+    each tuple is the alt text and url of any markdown images"""
+    # "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif -> [("rick roll", "https://i.imgur.com/aKaOqIh.gif")]
+    regex = r"!\[(.*?)\]\((.*?)\)"
+    matches = re.findall(regex, text)
+    return matches
+
+
+def extract_markdown_links(text):
+    """takes raw markdown text as input and returns a list of tuples,
+    each tuple is the anchor text and url of any markdown link"""
+    regex = r"\[(.*?)\]\((.*?)\)"
+    matches = re.findall(regex, text)
+    return matches
--- src/test_markdown_inline.py
+++ src/test_markdown_inline.py
@@ -1,7 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
-from markdown_inline import split_nodes_delimiter
+from markdown_inline import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 
 class TestMarkdownInline(unittest.TestCase):
     def test_split_nodes_delimiter_mid(self):
@@ -50,3 +50,21 @@ class TestMarkdownInline(unittest.TestCase):
                 TextNode("code block", TextType.CODE),
             ]
         )
+
+    def test_extract_markdown_images_one(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_images_multi(self):
+        matches = extract_markdown_images(
+            "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        )
+        self.assertListEqual([("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")], matches)
+
+    def test_extract_markdown_links_multi(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        )
+        self.assertListEqual([("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")], matches)
